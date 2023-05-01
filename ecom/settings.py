44518476ac41910b4e6d20527e5a318ec24b0c4d
“[NxAdx]” <[lokhandeaadarsh28@gmail.com]>
2023-05-01 08:58:07 +0530

    deploy

--- ecom/settings.py
+++ ecom/settings.py
@@ -5,7 +5,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 STRIPE_SECRET_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
 INSTALLED_APPS = [
     'django.contrib.admin',
